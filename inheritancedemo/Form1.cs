88c0ce71d7b56d80737bcfbdc53690443d4c2d1f
Revanth <revanth245790@gmail.com>
2025-03-04 11:39:28 +0530

    DAY4Projects

---
+++ inheritancedemo/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inheritancedemo
+{
+    public partial class Form1 : Form
+    {
+        //multi level
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class father
+        {
+            public void maruthicar()
+            {
+                MessageBox.Show("maruti Car...");
+            }
+        }
+        class son: father
+        {
+            public void MBcar()
+            {
+                MessageBox.Show("mercedes benz car....");
+            }
+        }
+        class grandson : son
+        {
+            public void BMWcar()
+            {
+                MessageBox.Show("BMW car...");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            grandson me = new grandson();
+            me.maruthicar();
+            me.MBcar();
+            me.BMWcar();
+        }
+    }
+}
---
+++ propertiesdemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propertiesdemo
+{
+    class Customer
+    {
+        private int customerid;
+        private string customername;
+        public void setcustomerid(int k)
+        {
+            this.customerid = k;
+        }
+        public void setcustomername(string name1)
+        {
+            this.customername = name1;
+        }
+        public int getcustomerid()
+        {
+            return this.customerid;
+        }
+        public string getcustomername()
+        {
+            return this.customername;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            c1.setcustomerid(101);
+            c1.setcustomername("sudha");
+            Console.WriteLine($"{c1.getcustomerid()}---{c1.getcustomername()}");
+            Console.ReadLine();
+        }
+    }
+}
